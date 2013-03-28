3af9427ff6c2af4a827b1fb96240a19927440d43
Ian Masliah <ian.masliah@u-psud.fr>
2013-03-28 16:15:37 +0100

    Clean-up and refurbishing of LU decomposition
    Implementation of lu now uses an architecture-dependent version
    of trf, which is a direct mapping to the underlying LAPACK kernel.
    This allows LU to use MAGMA and other specific version of BLAS/LAPACK.
    
    In addition, proper allocation avoiding scheme is now in place
    whenever possible for calls of tie(l,u,...) = lu(a). Previous code
    was overcopying in some situations (like non-square input).
    
    This change deprecates the factorization namespace as the internals
    of the factorization and the memory handling is done by trf or by
    specific version of LU itself. Functions depending on other members
    of the defunct lu_result type will be rewrote later using trf.

--- modules/core/linalg/include/nt2/toolbox/linalg/details/lapack/getrf.hpp
+++ modules/core/linalg/include/nt2/toolbox/linalg/details/lapack/getrf.hpp
@@ -9,7 +9,8 @@
 #ifndef NT2_TOOLBOX_LINALG_DETAILS_LAPACK_GETRF_HPP_INCLUDED
 #define NT2_TOOLBOX_LINALG_DETAILS_LAPACK_GETRF_HPP_INCLUDED
 #include <nt2/toolbox/linalg/details/utility/f77_wrapper.hpp>
-#include <nt2/toolbox/linalg/details/utility/workspace.hpp>
+
+/// TO DO DISABLE ENTIRELY LATER WHEN COMPLEX.LINALG IS DONE
 
 namespace nt2
 {
@@ -19,12 +20,6 @@ namespace nt2
     {
   void NT2_F77NAME(cgetrf)(const nt2_la_int* m, const nt2_la_int* n,
                        nt2_la_complex* a, const nt2_la_int* lda,
-                       nt2_la_int* ipiv, nt2_la_int* info);
-  void NT2_F77NAME(dgetrf)(const nt2_la_int* m, const nt2_la_int* n,
-                       double* a, const nt2_la_int* lda,
-                       nt2_la_int* ipiv, nt2_la_int* info);
-  void NT2_F77NAME(sgetrf)(const nt2_la_int* m, const nt2_la_int* n,
-                       float* a, const nt2_la_int* lda,
                        nt2_la_int* ipiv, nt2_la_int* info);
   void NT2_F77NAME(zgetrf)(const nt2_la_int* m, const nt2_la_int* n,
                        nt2_la_complex* a, const nt2_la_int* lda,
@@ -37,24 +32,12 @@ namespace nt2
                       T* a,                     \
                       const nt2_la_int* lda,    \
                       nt2_la_int* ipiv,         \
-                      nt2_la_int* info,         \
-               nt2::details::workspace<T> &)  \
-    {                                           \
-      NT2_F77NAME(NAME)(m,n,a,lda,ipiv,info);   \
-    }                                           \
-    inline void getrf(const nt2_la_int* m,      \
-                      const nt2_la_int* n,      \
-                      T* a,                     \
-                      const nt2_la_int* lda,    \
-                      nt2_la_int* ipiv,         \
                       nt2_la_int* info)         \
     {                                           \
       nt2::details::workspace<T> w;             \
       getrf(m, n, a, lda, ipiv, info, w);       \
     }                                           \
 
-    NT2_GETRF(sgetrf, float)
-    NT2_GETRF(dgetrf, double)
     NT2_GETRF(cgetrf, std::complex<float>)
     NT2_GETRF(zgetrf, std::complex<double>)
 
@@ -63,7 +46,3 @@ namespace nt2
 }
 
 #endif
-
-// /////////////////////////////////////////////////////////////////////////////
-// End of getrf.hpp
-// /////////////////////////////////////////////////////////////////////////////

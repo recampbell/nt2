f5cc403ca1bedb784c277759fa7407855980eb67
Joel Falcou <joel.falcou@lri.fr>
2013-03-31 14:35:37 +0200

    Reimplement det from trf instead of lu_result

--- modules/core/linalg/include/nt2/toolbox/linalg/functions/scalar/det.hpp
+++ modules/core/linalg/include/nt2/toolbox/linalg/functions/scalar/det.hpp
@@ -1,6 +1,7 @@
 //==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2003 - 2013   LASMEA UMR 6602 CNRS/Univ. Clermont II
 //         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2012 - 2013   MetaScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
@@ -9,10 +10,12 @@
 #ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_SCALAR_DET_HPP_INCLUDED
 #define NT2_TOOLBOX_LINALG_FUNCTIONS_SCALAR_DET_HPP_INCLUDED
 #include <nt2/toolbox/linalg/functions/det.hpp>
-#include <nt2/include/functions/lu.hpp>
-#include <nt2/include/constants/one.hpp>
-#include <nt2/include/constants/zero.hpp>
+#include <nt2/include/functions/trf.hpp>
+#include <nt2/include/functions/numel.hpp>
+#include <nt2/include/functions/diag_of.hpp>
 #include <nt2/include/functions/issquare.hpp>
+#include <nt2/include/functions/globalprod.hpp>
+#include <nt2/core/container/table/table.hpp>
 
 namespace nt2{ namespace ext
 {
@@ -21,13 +24,31 @@ namespace nt2{ namespace ext
                             , ((ast_<A0, nt2::container::domain>))
                             )
   {
-    typedef typename A0::value_type type_t;
-    typedef typename meta::as_real<type_t>::type rtype_t;
-    typedef typename meta::as_floating<rtype_t>::type result_type;
-    NT2_FUNCTOR_CALL(1)
+    typedef typename meta::as_real<typename A0::value_type>::type base;
+    typedef typename meta::as_floating<base>::type                result_type;
+
+    BOOST_FORCEINLINE result_type operator()(A0 const& a0) const
     {
-      BOOST_ASSERT_MSG(issquare(a0), "det for non square matrix");
-      return nt2::details::lu_result<A0>(a0).det();
+      BOOST_ASSERT_MSG( issquare(a0)
+                      , "DET: Argument must be a square matrix"
+                      );
+
+      nt2::container::table<typename A0::value_type>  lu(a0);
+      nt2::container::table<nt2_la_int>               ip;
+
+      // Factorize A as L/U
+      nt2_la_int  info = nt2::trf(lu,ip);
+
+      // DET(A) is the product of LU(A) diagonal by -1 at the power of
+      // the number of non-permutations done in LU(A)
+      std::size_t n     = nt2::numel(ip);
+      result_type sign  = One<result_type>();
+
+      // TODO: Parallelize this somehow ?
+      for(std::size_t i = 1;i <= n;++i)
+        sign  *= (ip(i) != nt2_la_int(i)) ? 1 : -1;
+
+      return nt2::globalprod(nt2::diag_of(lu))*sign;
     }
   };
 
@@ -37,12 +58,11 @@ namespace nt2{ namespace ext
                             )
   {
     typedef A0 result_type;
-    NT2_FUNCTOR_CALL(1)
+    BOOST_FORCEINLINE result_type operator()(A0 const& a0) const
     {
       return a0;
     }
   };
-
 } }
 
 #endif

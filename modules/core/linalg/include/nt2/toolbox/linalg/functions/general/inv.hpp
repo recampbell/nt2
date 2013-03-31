87ac77335a369f89648e332f0dedf5740842b75b
Joel Falcou <joel.falcou@lri.fr>
2013-03-31 14:37:27 +0200

    Reimplement inv usign trf and tri

--- modules/core/linalg/include/nt2/toolbox/linalg/functions/general/inv.hpp
+++ modules/core/linalg/include/nt2/toolbox/linalg/functions/general/inv.hpp
@@ -8,33 +8,57 @@
 //==============================================================================
 #ifndef NT2_TOOLBOX_LINALG_FUNCTIONS_GENERAL_INV_HPP_INCLUDED
 #define NT2_TOOLBOX_LINALG_FUNCTIONS_GENERAL_INV_HPP_INCLUDED
-
 #include <nt2/toolbox/linalg/functions/inv.hpp>
-#include <nt2/core/container/table/table.hpp>
-#include <nt2/include/functions/lu.hpp>
+#include <nt2/include/functions/trf.hpp>
+#include <nt2/include/functions/tri.hpp>
 #include <nt2/include/functions/assign.hpp>
-namespace nt2{ namespace ext
+#include <nt2/include/functions/extent.hpp>
+#include <nt2/include/functions/issquare.hpp>
+#include <nt2/sdk/meta/concrete.hpp>
+//#include <nt2/sdk/error/warning.hpp>
+#include <nt2/core/container/table/table.hpp>
+
+namespace nt2 { namespace ext
 {
   NT2_FUNCTOR_IMPLEMENTATION( nt2::tag::run_assign_, tag::cpu_
-                              , (A0)(A1)(N)
-                              , ((ast_<A0, nt2::container::domain>))
-                                ((node_<A1, nt2::tag::inv_, N,nt2::container::domain> ))
+                            , (A0)(A1)(N)
+                            , ((ast_<A0, nt2::container::domain>))
+                              ((node_ < A1
+                                      , nt2::tag::inv_
+                                      , N
+                                      , nt2::container::domain
+                                      >
+                              ))
                             )
   {
-    typedef typename boost::proto::result_of::child_c<A1&,0>::type       Out0;
-    typedef A0&                                                   result_type;
+    typedef A0& result_type;
+
     result_type operator()(A0& out, const A1& in) const
     {
-      out.resize(in.extent());
-      bool warn = choice(in, N());
-      choice(in, N());
-      out = boost::proto::child_c<0>(in);
-      out = nt2::details::lu_result<A0>(out).inv(warn);
-      return out;
+      BOOST_ASSERT_MSG( issquare(boost::proto::child_c<0>(in))
+                      , "INV: Argument must be a square matrix"
+                      );
+
+      // If needed, resize
+      out.resize(nt2::extent(in));
+
+      // Reuse output memory if possible
+      BOOST_AUTO_TPL( tmp, concrete(out) );
+      tmp = boost::proto::child_c<0>(in);
+
+      nt2::container::table<nt2_la_int> ip;
+      nt2_la_int                        lapack_info;
+
+      // Factorize A as L/U and call tri
+      nt2::trf(tmp,ip);
+      nt2::tri(tmp,ip);
+
+    // NT2_WARNING( (rcond(boost::proto::child_c<0>(in),tmp) >= nt2::Eps<base_t>())
+    //            , "INV: matrix is singular to machine precision."
+    //            );
+
+      return out = tmp;
     }
-  private :
-    static bool choice(const A1& in, boost::mpl::long_<1> const &){return true; }
-    static bool choice(const A1& in, boost::mpl::long_<2> const &){return boost::proto::child_c<1>(in); }
   };
 } }
 

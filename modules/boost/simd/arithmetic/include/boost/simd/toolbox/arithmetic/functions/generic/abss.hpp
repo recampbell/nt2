719498cc25bad3cc098d5ed958134040edd33ef3
jtlap <jtlapreste@gmail.com>
2013-04-11 08:48:19 +0200

    generic abss

--- modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/generic/abss.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/generic/abss.hpp
@@ -6,43 +6,52 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_ABSS_HPP_INCLUDED
-#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_ABSS_HPP_INCLUDED
+#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_GENERIC_ABSS_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_GENERIC_ABSS_HPP_INCLUDED
 #include <boost/simd/toolbox/arithmetic/functions/abss.hpp>
-#include <boost/simd/include/functions/simd/if_else.hpp>
+#include <boost/simd/include/functions/simd/abs.hpp>
+ #include <boost/simd/include/functions/simd/if_else.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::abss_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<unsigned_<A0>,X>))
+                            , (A0)
+                            , (generic_< unsigned_<A0> >)
                             )
   {
     typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1) { return a0; }
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+      return boost::simd::abs(a0);
+    }
   };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::abss_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<floating_<A0>,X>))
+                            , (A0)
+                            , (generic_< floating_<A0> >)
                             )
   {
     typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1) { return boost::simd::abs(a0); }
+    BOOST_SIMD_FUNCTOR_CALL(1)
+    {
+      return boost::simd::abs(a0);
+    }
   };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::abss_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<signed_<A0>,X>))
+                            , (A0)
+                            , (generic_< signed_<A0> >)
                             )
   {
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
       A0 a =  boost::simd::abs(a0);
-      return if_else(eq(a0,boost::simd::Valmin<A0>()), boost::simd::Valmax<A0>(), a);
+      return boost::simd::if_else(boost::simd::eq(a0,boost::simd::Valmin<A0>()),
+                                  boost::simd::Valmax<A0>(), a);
     }
   };
 } } }
+
 
 #endif

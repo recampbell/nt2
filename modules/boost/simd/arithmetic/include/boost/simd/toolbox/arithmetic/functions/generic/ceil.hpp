dd4c3e722423e6d088e436c569f70c42b164218a
jtlap <jtlapreste@gmail.com>
2013-04-11 17:15:46 +0200

    generic ceil

--- modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/generic/ceil.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/generic/ceil.hpp
@@ -6,40 +6,37 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_CEIL_HPP_INCLUDED
-#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_CEIL_HPP_INCLUDED
+#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_GENERIC_CEIL_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_GENERIC_CEIL_HPP_INCLUDED
+
 #include <boost/simd/toolbox/arithmetic/functions/ceil.hpp>
-#include <boost/simd/include/constants/one.hpp>
-#include <boost/simd/include/functions/simd/round.hpp>
 #include <boost/simd/include/functions/simd/seladd.hpp>
+#include <boost/simd/include/functions/simd/round.hpp>
+#include <boost/simd/include/functions/simd/is_less.hpp>
+#include <boost/simd/include/constants/one.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::ceil_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<arithmetic_<A0>,X>))
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::ceil_, tag::cpu_, (A0)
+                            , (generic_< arithmetic_<A0> >)
                             )
   {
     typedef A0 result_type;
-    BOOST_SIMD_FUNCTOR_CALL(1)
-    {
-      return a0;
-    }
+    BOOST_SIMD_FUNCTOR_CALL(1) { return a0; }
   };
 
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::ceil_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<floating_<A0>,X>))
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::ceil_, tag::cpu_, (A0)
+                            , (generic_< floating_<A0> >)
                             )
   {
     typedef A0 result_type;
+
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
       const A0 d0 = round(a0);
-      return seladd(lt(d0,a0),d0,One<A0>());
+      return boost::simd::seladd(boost::simd::lt(d0, a0),d0,boost::simd::One<A0>());
     }
   };
 } } }
-
 
 #endif

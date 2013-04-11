599a4dc6f880c2f32ad7e6ea73d727cc1091df27
jtlap <jtlapreste@gmail.com>
2013-04-11 16:17:43 +0200

    generic average

--- modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/generic/average.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/generic/average.hpp
@@ -6,39 +6,40 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_AVERAGE_HPP_INCLUDED
-#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SIMD_COMMON_AVERAGE_HPP_INCLUDED
+#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_GENERIC_AVERAGE_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_GENERIC_AVERAGE_HPP_INCLUDED
 #include <boost/simd/toolbox/arithmetic/functions/average.hpp>
+#include <boost/simd/include/constants/half.hpp>
+#include <boost/simd/include/functions/simd/shrai.hpp>
 #include <boost/simd/include/functions/simd/bitwise_and.hpp>
 #include <boost/simd/include/functions/simd/bitwise_xor.hpp>
-#include <boost/simd/include/functions/simd/plus.hpp>
-#include <boost/simd/include/functions/simd/multiplies.hpp>
-#include <boost/simd/include/functions/simd/shrai.hpp>
-#include <boost/simd/include/constants/half.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::average_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<arithmetic_<A0>,X>))((simd_<arithmetic_<A0>,X>))
-                            )
+                                   , (A0)
+                                   , (generic_< arithmetic_<A0> >)
+                                     (generic_< arithmetic_<A0> >)
+                                   )
   {
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
-      return b_and(a0, a1)+shrai(b_xor(a0, a1),1);
+      return boost::simd::bitwise_and(a0, a1)+
+        (boost::simd::shrai(boost::simd::bitwise_xor(a0, a1), 1));
     }
   };
 
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::average_, tag::cpu_
-                            , (A0)(X)
-                            , ((simd_<floating_<A0>,X>))((simd_<floating_<A0>,X>))
-                            )
+                                   , (A0)
+                                   , (generic_< floating_<A0> >)
+                                     (generic_< floating_<A0> >)
+                                   )
   {
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
     {
-       return (a0+a1)*Half<A0>();
+      return (a0+a1)*boost::simd::Half<result_type>();
     }
   };
 } } }
--- modules/boost/simd/arithmetic/unit/simd/average.cpp
+++ modules/boost/simd/arithmetic/unit/simd/average.cpp
@@ -51,3 +51,28 @@ NT2_TEST_CASE_TPL ( average_real__2_0,  BOOST_SIMD_SIMD_REAL_TYPES)
   NT2_TEST_ULP_EQUAL(average(boost::simd::One<vT>(), boost::simd::One<vT>())[0], boost::simd::One<T>(), 0);
   NT2_TEST_ULP_EQUAL(average(boost::simd::Zero<vT>(), boost::simd::Zero<vT>())[0], boost::simd::Zero<T>(), 0);
 } // end of test for floating_
+NT2_TEST_CASE_TPL ( average_int__2_0,  BOOST_SIMD_INTEGRAL_TYPES)
+{
+  using boost::simd::average;
+  using boost::simd::tag::average_;
+  using boost::simd::load;
+  using boost::simd::native;
+  using boost::simd::meta::cardinal_of;
+  typedef BOOST_SIMD_DEFAULT_EXTENSION  ext_t;
+  typedef typename boost::dispatch::meta::upgrade<T>::type   u_t;
+  typedef native<T,ext_t>                        n_t;
+  typedef n_t                                     vT;
+  typedef typename boost::dispatch::meta::as_integer<T>::type iT;
+  typedef native<iT,ext_t>                       ivT;
+  typedef typename boost::dispatch::meta::call<average_(vT,vT)>::type r_t;
+  typedef typename boost::simd::meta::scalar_of<r_t>::type sr_t;
+  typedef typename boost::simd::meta::scalar_of<r_t>::type ssr_t;
+
+  // specific values tests
+
+  NT2_TEST_ULP_EQUAL(average(boost::simd::Mone<vT>(), boost::simd::Mone<vT>())[0], boost::simd::Mone<T>(), 0);
+  NT2_TEST_ULP_EQUAL(average(boost::simd::One<vT>(), boost::simd::One<vT>())[0], boost::simd::One<T>(), 0);
+  NT2_TEST_ULP_EQUAL(average(boost::simd::Zero<vT>(), boost::simd::Zero<vT>())[0], boost::simd::Zero<T>(), 0);
+  NT2_TEST_ULP_EQUAL(average(boost::simd::Zero<vT>(), boost::simd::One<vT>())[0], boost::simd::Zero<T>(), 0);
+  NT2_TEST_ULP_EQUAL(average(boost::simd::Two <vT>(), boost::simd::One<vT>())[0], boost::simd::One<T>(), 0);
+} // end of test for floating_

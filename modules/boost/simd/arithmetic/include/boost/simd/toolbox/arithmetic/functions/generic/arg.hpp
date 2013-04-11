ea1773b69b22405a95d2d253412259e22dc21144
jtlap <jtlapreste@gmail.com>
2013-04-11 09:08:49 +0200

    generic arg and bench correction

--- modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/generic/arg.hpp
+++ modules/boost/simd/arithmetic/include/boost/simd/toolbox/arithmetic/functions/generic/arg.hpp
@@ -6,28 +6,31 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SCALAR_ARG_HPP_INCLUDED
-#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_SCALAR_ARG_HPP_INCLUDED
+#ifndef BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_GENERIC_ARG_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_ARITHMETIC_FUNCTIONS_GENERIC_ARG_HPP_INCLUDED
 #include <boost/simd/toolbox/arithmetic/functions/arg.hpp>
 #include <boost/simd/include/constants/pi.hpp>
-#include <boost/simd/include/constants/zero.hpp>
-#include <boost/simd/include/functions/scalar/is_gez.hpp>
-#include <boost/simd/include/functions/scalar/is_ltz.hpp>
-#include <boost/simd/include/functions/scalar/is_nan.hpp>
+#include <boost/simd/include/functions/simd/is_nan.hpp>
+#include <boost/simd/include/functions/simd/is_ltz.hpp>
+#include <boost/simd/include/functions/simd/if_else_zero.hpp>
+#include <boost/simd/include/functions/simd/if_allbits_else.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::arg_, tag::cpu_
                             , (A0)
-                            , (scalar_< floating_<A0> >)
+                            , (generic_< floating_<A0> >)
                             )
   {
     typedef A0 result_type;
     BOOST_SIMD_FUNCTOR_CALL(1)
     {
-      if (is_nan(a0)) return a0;
-      if (is_gez(a0)) return Zero<A0>();
-      return Pi<A0>();
+      // a0 >= 0 -> 0, a0 < 0 ->Pi, a0 Nan -> Nan
+      return nt2::if_nan_else(nt2::is_nan(a0),
+                              nt2::if_else_zero(nt2::is_ltz(a0),
+                                                nt2::Pi<result_type>()
+                                               )
+                             );
     }
   };
 } } }
